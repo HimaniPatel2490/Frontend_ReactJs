551d22acd46e19125b949ae0aedd8be5611c385f
HimaniPatel2490 <himanipatel2803@gmail.com>
2025-04-28 11:14:09 +0530

    js assignment

---
+++ React/react-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+    return(
+        <div>
+            <class_component/>
+            <Function_component/>
+
+        </div>
+    )
+}
+export default App;
---
+++ React/react-demo/src/Component/Class_Component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Class_Component extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Himani</h2>
+      </div>
+    );
+  }
+}
