2ac89b3f8d6c29243e2cf13d56e8823c07572f1f
HimaniPatel2490 <himanipatel2803@gmail.com>
2025-05-14 18:50:46 +0530

    js assignment

---
+++ React/demo-vite/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className="text-2xl font-sans font-black ">Hello this h1 tag </h1>
+      <h2 className="text-5xl font-serif italic font-medium">Hello this h2 data</h2>
+      <h2 className="text-4xl font-mono">Hello this h2 data</h2>
+      <h2 className="text-5xl">Hello this h2 data</h2>
+      <h2 className="text-5xl">Hello this h2 data</h2>
+      <h2 className="text-5xl">Hello this h2 data</h2>
+
+      <ul className="list-image-[url(demo.png)]">
+        <li>home</li>
+        <li>home</li>
+        <li>home</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
