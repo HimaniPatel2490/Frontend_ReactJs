3cedc5a2c97447218d509578a9e305e867df9d98
HimaniPatel2490 <himanipatel2803@gmail.com>
2025-05-22 16:07:23 +0530

    assignment

---
+++ Assignment/ReactJS/reactjs/src/App.js
@@ -0,0 +1,14 @@
+// import react from "react";
+import Function_compo from "./Basic/Function_compo";
+import JavascriptXML from "./JSX/JavascriptXML";
+
+function App() {
+  return (
+    <div>
+      <Function_compo/>
+      <JavascriptXML />
+    </div>
+  );
+}
+
+export default App;
---
+++ Assignment/ReactJS/reactjs/src/Basic/Function_compo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Function_compo() {
+  return (
+    <div>
+      <h1>Hello React...</h1>
+    </div>
+  );
+}
+
+export default Function_compo;
